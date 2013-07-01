a343da57c2407e08c03659b1445f9ea3b562d225
elizahyde <elizabeth@elizahyde.com>
2013-07-01 10:58:05 -0700

    first commit

---
+++ js/game_actions.js
@@ -0,0 +1,5 @@
+// Click a tile 1. Activate tile unless already active. Count click.
+
+// If selection length == 2, AND no match, activate tile unless already active. Count click.
+
+// If selection length == 2, AND match, fade out, no longer selectable. Count click.
