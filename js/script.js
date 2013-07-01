72ea3229bb984447d995e16fd4307d87ac3417be
elizahyde <elizabeth@elizahyde.com>
2013-07-01 14:03:06 -0700

    adding js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-  console.log(':)')
-})
+
+var tiles = $('.tile');
+var num_clicks = 0;
+
+var handle_click = function() {
+  var tile = $(this);
+  tile.addClass('active');
+}
+
+$.each(tiles, function(index, tile) {
+  var tile = $(tile);
+  tile.on('click', handle_click);
+});
+
+
+});
